141c2944dba049d726caa3404c3c98c4ecd2a4d9
Gabriel Denis López <gabrieldenislopez@gmail.com>
2021-01-24 19:00:51 -0300

    Form, Post & Post Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import memoirs from './images/memoirs.png';
 
 const App = () => {
   return (
-    <div>
-      <h1>HEY</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memoirs
+        </Typography>
+        <img src={memoirs} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form />
+          </Grid>
+        </Grid>
+      </Grow>
+    </Container>
   );
 };
 
