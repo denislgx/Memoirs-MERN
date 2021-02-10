63d8b5aea78e54f421b08df2c3b14ca537e5b96d
Gabriel Denis López <gabrieldenislopez@gmail.com>
2021-02-10 19:11:34 -0300

    Update: Delete & Update Only Your Own Posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -66,15 +66,18 @@ const Post = ({ post, currentId, setCurrentId }) => {
         </Typography>
       </div>
 
-      <div className={classes.overlay2}>
-        <Button
-          style={{ color: 'white' }}
-          size="small"
-          onClick={() => setCurrentId(post._id)}
-        >
-          <MoreHorizIcon fontSize="default" />
-        </Button>
-      </div>
+      {(user?.result?.googleId === post?.creator ||
+        user?.result?._id === post?.creator) && (
+        <div className={classes.overlay2}>
+          <Button
+            style={{ color: 'white' }}
+            size="small"
+            onClick={() => setCurrentId(post._id)}
+          >
+            <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+      )}
 
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary">
@@ -105,14 +108,17 @@ const Post = ({ post, currentId, setCurrentId }) => {
           <Likes />
         </Button>
 
-        <Button
-          size="small"
-          color="primary"
-          onClick={() => dispatch(deletePost(post._id))}
-        >
-          <DeleteIcon fontSize="small" />
-          Delete
-        </Button>
+        {(user?.result?.googleId === post?.creator ||
+          user?.result?._id === post?.creator) && (
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => dispatch(deletePost(post._id))}
+          >
+            <DeleteIcon fontSize="small" />
+            Delete
+          </Button>
+        )}
       </CardActions>
     </Card>
   );
